a245d9b52d175fc5aec7fca65e429d331a8b7425
maonster123 <2492603854@qq.com>
2026-06-28 21:56:24 +0800

    disable turbopack build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
+  turbopack: { build: false },
 };
 
 export default nextConfig;
